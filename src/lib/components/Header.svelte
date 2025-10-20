3ea835b5d7f87aaa80ef1a02164d03d569bee5df
Qofy <148817944+Qofy@users.noreply.github.com>
2025-10-20 11:51:10 +0200

    feat: enhance header component with navigation links and update global styles for improved typography

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,11 +6,64 @@
 
 <header>
   <h1>Intuivo !</h1>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">about us</a>
+      </li>
+      <li>
+        <a href="#">portfolio</a>
+      </li>
+      <li>
+        <a href="#">contact</a>
+      </li>
+    </ul>
+  </nav>
 </header>
 
 <style>
-header{
-  background-color: beige;
-  width: 100%;
+header {
+  margin-bottom: 4rem;
+  /* max-width: 900px; */
+  margin: auto;
+  /* background-color: #302e2f; */
+  background-color: #171717;
+  height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1.5rem;
 }
+
+header:hover{
+  background-color: #171717;
+}
+
+ header h1{
+ /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
+ font-size: 3rem; 
+ cursor: pointer;
+ font-family: "Playwrite DE SAS", cursive;
+}
+
+ul{
+  list-style: none;
+}
+nav ul{
+  display: flex;
+  gap: 4rem;
+}
+
+nav a{
+  text-decoration: none;
+  font-size: 2.2rem;
+  font-weight: 700;
+  font-family: "Playwrite DE SAS", cursive;
+  color: inherit;
+}
+
+nav a:hover{
+  text-decoration: underline;
+}
+
 </style>
