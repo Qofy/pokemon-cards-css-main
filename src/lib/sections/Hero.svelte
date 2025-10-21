43835438e7baf79cfcd9a540bca8c117ae829053
Qofy <148817944+Qofy@users.noreply.github.com>
2025-10-21 05:16:03 +0200

    feat: update global styles and enhance Hero section with content and audio controls

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -4,11 +4,41 @@
 
 
 <main>
-<h1>
-  hero section
-</h1>
+  <h1>Intuivo !</h1>
+  <a href="#">
+    (/inˈto͞o-i-vō/)
+  </a>
+  <h3>
+    Igniting Creative Ideas.
+  </h3>
+  <audio class="audiointuivo" controls="true">
+    <source src="https://intuivo.com/2016/sounds/intuivo.mp3" type="audio/mpeg">
+  </audio>
 </main>
 
 <style>
-  /* your styles go here */
+  main{
+    height: 100%;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ margin: 40px auto;
+  }
+
+  h1{
+ /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
+ font-size: 3rem; 
+ cursor: pointer;
+ font-family: "Playwrite DE SAS", cursive;
+}
+
+.audiointuivo{
+  width: 60rem;
+		box-shadow: 0 4px 24px 0 #a0bd35, 0 1.5px 6px 0 #87a909;
+}
+
+.audiointuivo:hover{
+  transform: scale(1.1);
+  transition: all 0.4s cubic-bezier(0.4, 0, 0.2, 1);
+}
 </style>
